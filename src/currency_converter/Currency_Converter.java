0a663ff0155ae7f8ee659bdd2d7e3de07934baf2
BarahtianR <barahtian.rares@gmail.com>
2021-07-17 15:49:13 +0300

    A program that converts a specific sum of a given currency to another,given,currency

---
+++ src/currency_converter/Currency_Converter.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package currency_converter;
+
+/**
+ *
+ * @author Rares
+ */
+public class Currency_Converter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        currency_1 cur = new currency_1();
+        cur.setVisible(true);
+    }
+    
+}
